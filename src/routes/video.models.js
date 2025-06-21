4cafca594db77992ebf56cb9ad9a48b6e379172e
jaxx770g <jays86922@gmail.com>
2025-06-21 20:39:01 +0530

    this jwt token models

---
+++ src/routes/video.models.js
@@ -0,0 +1,43 @@
+
+import mongoose from 'mongoose';
+import mongooseAggregatePaginate from "mongoose-aggregate_paginate-v2"
+const videoSchema=new mongoose.Schema({
+  videoFile:{
+    type:String,
+    required:true
+  },
+  title:{
+   type:String,
+   required:true
+  },
+  description:{
+    type:String,
+    required:true
+  },
+  duration:{
+    type:Number,
+    required:true
+  },
+  views:{
+    type:Number,
+    required:true,
+  },
+  isPublished:{
+    type:Boolean,
+    defauly:true
+  },
+  owner:{
+    type:Schema.Type.ObjectId,
+    ref:"User"
+  }
+
+
+
+
+
+},{
+    timestamps:true
+})
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video=mongoose.model("Video",videoSchema)
